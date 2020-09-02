ae6deac814c4388a970668ae9874ba1e8fc1337b
Linus Pahl <linus@graylog.com>
2020-09-02 11:18:41 +0200

    Refactor EmptyEntity component

--- graylog2-web-interface/src/components/common/EmptyEntity.jsx
+++ graylog2-web-interface/src/components/common/EmptyEntity.jsx
@@ -1,40 +1,52 @@
-import React from 'react';
+// @flow strict
+import * as React from 'react';
 import PropTypes from 'prop-types';
+import styled, { type StyledComponent } from 'styled-components';
 
-import style from './EmptyEntity.css';
+import { type ThemeInterface } from 'theme';
 
 /**
  * Component used to represent an empty entity in Graylog. This component allows us to display some larger
  * text to the user explaining what that entity is and a link to create a new one.
  */
-class EmptyEntity extends React.Component {
-  static propTypes = {
-    /** Text or node to be rendered as title. */
-    title: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.node,
-    ]),
-    /**
-     * Any other content the component should display below the title. This may include a description and button
-     * or link to easily create a new entity.
-     */
-    children: PropTypes.node.isRequired,
-  };
 
-  static defaultProps = {
-    title: 'Looks like there is nothing here, yet!',
-  };
+const Container: StyledComponent<{}, ThemeInterface, HTMLDivElement> = styled.div(({ theme }) => `
+  font-size: ${theme.fonts.size.body};
+  text-align: center;
+`);
 
-  render() {
-    const { children, title } = this.props;
+const Headline = styled.h2`
+  margin-top: 5px;
+  margin-bottom: 10px;
+`;
 
-    return (
-      <div className={style.component}>
-        <h3>{title}</h3>
-        {children}
-      </div>
-    );
-  }
-}
+type Props = {
+  children: React.Node,
+  title: string,
+};
+
+const EmptyEntity = ({ children, title }: Props) => (
+  <Container>
+    <Headline>{title}</Headline>
+    {children}
+  </Container>
+);
+
+EmptyEntity.propTypes = {
+  /** Text or node to be rendered as title. */
+  title: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.node,
+  ]),
+  /**
+   * Any other content the component should display below the title. This may include a description and button
+   * or link to easily create a new entity.
+   */
+  children: PropTypes.node.isRequired,
+};
+
+EmptyEntity.defaultProps = {
+  title: 'Looks like there is nothing here, yet!',
+};
 
 export default EmptyEntity;
